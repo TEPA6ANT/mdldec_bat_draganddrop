500bff887b3094ce27120ac3057ab16b77eb5e24
T'era <108683190+TEPA6ANT@users.noreply.github.com>
2024-05-05 01:07:48 +0800

    Add files via upload

---
+++ mdldec.bat
@@ -0,0 +1,28 @@
+@echo off
+
+if "%~1"=="" (
+    echo FUCK YOU!!! YOUR FUCKIN DICK!!!
+    pause
+    exit /b
+)
+
+type nul > activities.txt
+cd /d %~dp1
+%~dp0mdldec_win32.exe %~nx1
+
+cd /d %~dp1
+mkdir orig
+
+for /f "tokens=*" %%f in ('dir /b /a-d *mdl') do (
+    if "%%~nf"=="%%~nf.mdl" (
+        move /y "%%f" orig
+    ) else (
+        for /l %%i in (0 1 99) do (
+            if exist "%%f" move /y "%%f" orig
+        )
+        if exist "t.mdl" move /y "t.mdl" orig
+    )
+)
+
+if exist activities.txt del activities.txt
+pause
